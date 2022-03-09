8ec30a5060028cec9cd9e54e29057cdf89189438
AndhikaRahmatullah <dikarahmatullah909@gmail.com>
2022-03-10 04:02:54 +0700

    first commit

---
+++ public/script.js
@@ -0,0 +1,10 @@
+const joinNow = document.querySelector(`#joinNow`);
+joinNow.addEventListener(`click`, function () {
+	alert(`You click " Join Now " !`)
+});
+
+
+const submit = document.querySelector(`#submit`);
+submit.addEventListener(`click`, function () {
+	alert(`You click " Submit " !`)
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "judul": ["Sansita Swashed", "cursive"],
+        "subJudul": ["Source Serif Pro", "serif"],
+        "paragraf": ["Roboto Slab", "serif"]
+      }
+    },
+  },
+  plugins: [],
+}
