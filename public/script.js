4b34b88cfa47a25c66f18f6c79d8f66c861b680a
AndhikaRahmatullah <dikarahmatullah909@gmail.com>
2022-03-12 01:03:37 +0700

    tambah promt di submit akhir

--- public/script.js
+++ public/script.js
@@ -6,5 +6,35 @@ joinNow.addEventListener(`click`, function () {
 
 const submit = document.querySelector(`#submit`);
 submit.addEventListener(`click`, function () {
-	alert(`You click " Submit " !`)
+	// 	const email = document.querySelector(`#email`);
+	// 	const nama = document.querySelector(`#nama`);
+	// 	const pesan = document.querySelector(`#pesan`);
+
+	// 	alert(`Email : ${email.value}
+	// Nama : ${nama.value}
+	// Pesan : ${pesan.value}
+
+	// Pesan anda sukses terkirim! 
+	// Terimakasih telah berkunjung di Portfolio Portfolio-an Dika.`);
+
+	let prom = Number(prompt(`Beri Nilai (1 - 10) Untuk Website Ini : `));
+	console.log(prom);
+	if ((prom >= 0) && (prom <= 4)) {
+		alert(`Hmm.. Ga apa apa, Dika akan bekerja lebih baik lagi. Terimakasih ya !`)
+	}
+	else if ((prom >= 5) && (prom <= 7)) {
+		alert(`Baik, Terimakasih atas penilaiannya !`)
+	}
+	else if ((prom >= 8) && (prom <= 10)) {
+		alert(`WOWW ! Terimakasih atas penilaiannya !`)
+	}
+	else if ((prom <= 0) || (prom >= 11)) {
+		alert(`1 sampai 10 please bro sis !`)
+	}
+
 });
+
+// for (let a = 0; a < array.length; a++) {
+// 	const element = array[a];
+
+// }
